c7723eb44396def7c0b2b5d2718fdb850f88e6d7
Steve Klabnik <steve@steveklabnik.com>
2016-10-21 17:12:27 -0400

    more stuff

---
+++ mem.rb
@@ -0,0 +1,29 @@
+require 'objspace'
+require 'pp'
+
+puts "OBJECT SIZE"
+
+size = ObjectSpace.memsize_of(Object::new)
+pp size
+
+def allocate_count
+  GC.disable
+  before = ObjectSpace.count_objects
+  yield
+  after = ObjectSpace.count_objects
+  after.each { |k,v| after[k] = v - before[k] }
+  GC.enable
+  after
+end
+
+counts = allocate_count {
+  /\A[[:space:]]*\z/ === "Привет";
+}
+
+counts.delete(:FREE)
+
+puts "COUNTS"
+pp counts
+
+puts "MATH:"
+pp (counts.values.inject(0, :+) * 40)
